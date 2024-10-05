bc4a878aea691976c2213602e6540695d6f0d2fc
Kenta Shimosawa <shimon101018@gmail.com>
2024-10-06 00:49:13 +0900

    feat: 🎸 add Work component

---
+++ components/work/work.component.tsx
@@ -0,0 +1,15 @@
+import * as Styles from "./work.styles";
+
+export const Work = () => {
+  return (
+    <Styles.Container>
+      <Styles.Title>Work</Styles.Title>
+      <Styles.Text>
+        新卒でエンジニアになり、2年間正社員エンジニアを経験しました。その後はフリーランスとして活動しています。
+        正社員の時はWebフロントエンド、バックエンド、インフラ、Windowsアプリ、モバイルアプリ、スマートウォッチアプリ等、領域問わず開発に携わりました。
+        その時にプロジェクトマネージャや開発責任者の役割も経験しています。
+        独立してからは主にWebフロントエンドに特化しています。直近ではNext.jsをメインで使っています。
+      </Styles.Text>
+    </Styles.Container>
+  );
+};
