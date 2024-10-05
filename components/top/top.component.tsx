e75ad87b458140a9a6b8bb96512d893cdc0b8f78
Kenta Shimosawa <shimon101018@gmail.com>
2024-10-05 23:10:49 +0900

    feat: 🎸 wip add top component

---
+++ components/top/top.component.tsx
@@ -0,0 +1,10 @@
+import { Hero } from "../hero";
+import * as Styles from "./top.styles";
+
+export const Top = () => {
+  return (
+    <Styles.Container>
+      <Hero />
+    </Styles.Container>
+  );
+};
