6a162ed95ddd9d683347df2ccaeb6534c91d29d0
Kenta Shimosawa <shimon101018@gmail.com>
2024-10-07 21:32:05 +0900

    test: 💍 update tests theme provider

--- components/footer/footer.component.spec.tsx
+++ components/footer/footer.component.spec.tsx
@@ -1,14 +1,11 @@
 import { render } from "@testing-library/react";
 import { expect, test } from "vitest";
 import { Footer } from "./footer.component";
-import { ThemeProvider } from "styled-components";
-import { theme } from "@/theme";
+import { ThemeProvider } from "@/theme";
 
 test("should render", () => {
   const { asFragment } = render(<Footer />, {
-    wrapper: ({ children }) => (
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    ),
+    wrapper: ({ children }) => <ThemeProvider>{children}</ThemeProvider>,
   });
 
   expect(asFragment()).toMatchSnapshot();
